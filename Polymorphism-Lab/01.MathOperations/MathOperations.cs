a44b2265b154a674f2caaf255d42dde047b2f350
theoners <theonebgrs@gmail.com>
2019-03-22 00:29:06 +0100

    added Polymorphism-Lab

---
+++ Polymorphism-Lab/01.MathOperations/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿namespace Operations
+{
+   public class MathOperations
+    {
+
+        public int Add(int firstNumber, int secondNumber)
+        {
+            var result = firstNumber + secondNumber;
+            return result;
+        }
+
+        public double Add(double firstNumber, double secondNumber, double thirdNumber)
+        {
+            var result = firstNumber + secondNumber + thirdNumber;
+            return result;
+        }
+
+        public decimal Add(decimal firstNumber, decimal secondNumber, decimal thirdNumber)
+        {
+            var result = firstNumber + secondNumber + thirdNumber;
+            return result;
+        }
+    }
+}
---
+++ Polymorphism-Lab/02.Animals/Animal.cs
@@ -0,0 +1,21 @@
+﻿namespace Animals
+{
+    public abstract class Animal
+    {
+        private string name;
+
+        private string favoriteFood;
+
+        protected Animal(string name, string favoriteFood)
+        {
+            this.name = name;
+            this.favoriteFood = favoriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.name} and my fovourite food is {this.favoriteFood}";
+        }
+
+    }
+}
