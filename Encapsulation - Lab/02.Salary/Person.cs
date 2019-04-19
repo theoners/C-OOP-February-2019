fe20778b4f98fa78881b8826856a9120e0ac650a
theoners <theonebgrs@gmail.com>
2019-04-19 12:46:01 +0200

    added Encapsulation - Lab

---
+++ Encapsulation - Lab/02.Salary/Person.cs
@@ -0,0 +1,38 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age , decimal salary)
+        {   
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get;  private set; }
+
+        public int Age { get;  private set; }
+
+        public decimal Salary { get;  private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            var percent = percentage / 100;
+            if (this.Age>30)
+            {
+                this.Salary += this.Salary * percent;
+            }
+            else
+            {
+                this.Salary += this.Salary * percent / 2;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:F2} leva.";
+        }
+    }
+}
---
+++ Encapsulation - Lab/04.Team/Team.cs
@@ -0,0 +1,36 @@
+﻿namespace PersonsInfo
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Team
+    {
+        private string name;
+        private List<Person> firstTeam;
+        private List<Person> reserveTeam;
+
+        public Team(string name)
+        {
+            this.name = name;
+            this.firstTeam= new List<Person>();
+            this.reserveTeam= new List<Person>();
+        }
+
+        public List<Person> FirstTeam => this.firstTeam.AsReadOnly().ToList();
+
+        public List<Person> ReserveTeam => this.reserveTeam.AsReadOnly().ToList();
+
+        public void AddPlayer(Person person)
+        {
+            if (person.Age<40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+
+    }
+}
