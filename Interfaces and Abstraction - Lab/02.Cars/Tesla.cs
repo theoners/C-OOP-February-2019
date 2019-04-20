5a927e20e964cd226967021ef86be9fe0af72343
theoners <theonebgrs@gmail.com>
2019-04-20 22:02:23 +0200

    added Interfaces and Abstraction - Lab

---
+++ Interfaces and Abstraction - Lab/02.Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿namespace Cars
+{
+    using System;
+
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries{Environment.NewLine}" +
+                   $"{this.Start()}{Environment.NewLine}" +
+                   $"{this.Stop()}";
+        }
+    }
+}
